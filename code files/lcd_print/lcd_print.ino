5b337e9419c039898c24511aa5ad4f39f526d108
Yebin Lee <71310074+YebinLeee@users.noreply.github.com>
2021-02-24 09:36:07 +0900

    lcd print

--- code files/lcd_print/lcd_print.ino
+++ code files/lcd_print/lcd_print.ino
@@ -1,14 +1,13 @@
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 
-LiquidCrystal_I2C lcd(0x27,16,2); // 주소, 열, 행
+LiquidCrystal_I2C lcd(0x27,16,2); // lcd 객체 생성(주소, 열, 행)
 
 void setup()
 {
   lcd.init();
   lcd.backlight();
-  lcd.print("Hello, I'm Yebin hehe");
- 
+  lcd.print("Hello, I'm Yebin hehe"); // lcd 화면에 문자열 출력(16번째 문자열에서 끝남)
 }
 
 void loop() {
