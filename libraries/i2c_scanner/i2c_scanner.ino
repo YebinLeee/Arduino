c6deca6f20220437d1fbec5b6c51ff1ffa065c67
Yebin Lee <71310074+YebinLeee@users.noreply.github.com>
2021-03-03 02:36:32 +0900

    i2c scanner

---
+++ libraries/i2c_scanner/i2c_scanner.ino
@@ -0,0 +1,53 @@
+#include <LiquidCrystal.h>
+#include <Wire.h>
+
+LiquidCrystal_I2C lcd(0x27,16,2); // 주소, 열, 행
+
+void setup()
+{
+  Wire.begin();
+ 
+  Serial.begin(115200);     //시리얼 통신 속도(메가보드 115200)
+  Serial.println("\nI2C Scanner");
+}
+ 
+ 
+void loop()
+{
+  byte error, address;
+  int nDevices;
+ 
+  Serial.println("Scanning...");
+ 
+  nDevices = 0;
+  for(address = 1; address < 127; address++ ) 
+  {
+ 
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+ 
+    if (error == 0)
+    {
+      Serial.print("I2C device found at address 0x");
+      if (address<16) 
+        Serial.print("0");
+      Serial.print(address,HEX);
+      Serial.println("  !");
+ 
+      nDevices++;
+    }
+    else if (error==4) 
+    {
+      Serial.print("Unknow error at address 0x");
+      if (address<16) 
+        Serial.print("0");
+      Serial.println(address,HEX);
+    }    
+  }
+  if (nDevices == 0)
+    Serial.println("No I2C devices found\n");
+  else
+    Serial.println("done\n");
+ 
+  delay(5000);           // wait 5 seconds for next scan
+}
